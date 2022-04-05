c4d92cd06e90aa68110a2304cf14e9adbb7cd213
Starunov Sergei <35844513@bk.ru>
2022-04-05 15:19:39 +0300

    homework for lesson_7

---
+++ lesson_7/Starunon_Sergei_dz_5.py
@@ -0,0 +1,19 @@
+import os
+import json
+
+find_in_folder = './some_data/'
+result_dict = {}
+
+for root, dirnames, filenames in os.walk(find_in_folder):
+    for filename in filenames:
+        path_file = os.path.join(root, filename)
+        digits = len(str(os.stat(path_file).st_size))
+        file_extension = os.path.splitext(filename)[-1]
+        file_quantity, extensions = result_dict.get(10 ** digits, (0, []))
+        file_quantity += 1
+        if not (file_extension in extensions):
+            extensions.append(file_extension)
+        result_dict[10 ** digits] = (file_quantity, extensions)
+
+with open(os.path.join(os.getcwd(), 'summary.json'), 'w', encoding='utf-8') as file:
+    json.dump(result_dict, file)
